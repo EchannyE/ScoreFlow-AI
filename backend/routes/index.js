8e0e396b8640f66cde380fd89685280989448391
EchannyE <ondaechanny@outlook.com>
2026-03-30 21:07:02 +0100

    Refactor index.js to use ES6 import/export syntax

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const campaignRoutes = require('./campaignRoutes');
-const evaluationRoutes = require('./evaluation.routes');
-const notificationRoutes = require('./notification.routes');
-const submissionRoutes = require('./submissionRoutes');
-const userRoutes = require('./userRoutes');
+import express from 'express'
+import campaignRoutes from './campaignRoutes.js'
+import evaluationRoutes from './evaluation.routes.js'
+import notificationRoutes from './notification.routes.js'
+import submissionRoutes from './submissionRoutes.js'
+import userRoutes from './userRoutes.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.use('/campaigns', campaignRoutes);
-router.use('/evaluations', evaluationRoutes);
-router.use('/notifications', notificationRoutes);
-router.use('/submissions', submissionRoutes);
-router.use('/users', userRoutes);
+router.use('/campaigns', campaignRoutes)
+router.use('/evaluations', evaluationRoutes)
+router.use('/notifications', notificationRoutes)
+router.use('/submissions', submissionRoutes)
+router.use('/users', userRoutes)
 
-module.exports = router;
+export default router

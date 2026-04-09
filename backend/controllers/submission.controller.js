24023c31eba720423fe8237df9a007b3e985ae3f
EchannyE <ondaechanny@outlook.com>
2026-04-09 16:36:38 +0100

    Refactor submission controller for improved readability

--- backend/controllers/submission.controller.js
+++ backend/controllers/submission.controller.js
@@ -1,41 +1,62 @@
 import * as submissionService from '../services/submission.service.js'
-import { ok, fail }             from '../utils/aiResponse.js'
- 
+import { ok, fail } from '../utils/aiResponse.js'
+
 export const list = async (req, res) => {
   try {
     const filters = {}
+
     if (req.query.campaignId) filters.campaignId = req.query.campaignId
-    if (req.query.status)     filters.status     = req.query.status
-    if (req.query.flagged)    filters.flagged     = req.query.flagged === 'true'
+    if (req.query.status) filters.status = req.query.status
+    if (req.query.flagged) filters.flagged = req.query.flagged === 'true'
+
     ok(res, { submissions: await submissionService.list(filters) })
-  } catch (e) { fail(res, e) }
+  } catch (e) {
+    fail(res, e)
+  }
 }
- 
+
 export const get = async (req, res) => {
-  try { ok(res, await submissionService.getById(req.params.id)) }
-  catch (e) { fail(res, e) }
+  try {
+    ok(res, await submissionService.getById(req.params.id))
+  } catch (e) {
+    fail(res, e)
+  }
 }
- 
+
 export const mine = async (req, res) => {
-  try { ok(res, await submissionService.mine(req.user._id)) }
-  catch (e) { fail(res, e) }
+  try {
+    ok(res, await submissionService.mine(req.user._id))
+  } catch (e) {
+    fail(res, e)
+  }
 }
- 
+
 export const create = async (req, res) => {
   try {
-    const userInfo = { submitterEmail: req.user.email, submitterName: req.user.name }
-    ok(res, await submissionService.create(req.body, req.user._id, userInfo), 201)
-  } catch (e) { fail(res, e) }
+    ok(res, await submissionService.create(req.body, req.user._id), 201)
+  } catch (e) {
+    fail(res, e)
+  }
 }
- 
+
 export const update = async (req, res) => {
-  try { ok(res, await submissionService.update(req.params.id, req.body)) }
-  catch (e) { fail(res, e) }
+  try {
+    ok(res, await submissionService.update(req.params.id, req.body))
+  } catch (e) {
+    fail(res, e)
+  }
 }
 
 export const assign = async (req, res) => {
   try {
-    ok(res, await submissionService.assignEvaluator(req.params.id, req.body.evaluatorId))
-  } catch (e) { fail(res, e) }
+    ok(
+      res,
+      await submissionService.assignEvaluator(
+        req.params.id,
+        req.body.evaluatorId
+      )
+    )
+  } catch (e) {
+    fail(res, e)
+  }
 }
- 
